ae6ec21b77dc1384e0c3247803785ff452d0cf5c
y-chan <y-chan@y-chan.dev>
2021-08-03 11:50:58 +0900

    add linux wrapper

---
+++ aqkanji2koe/linux.cpp
@@ -0,0 +1,105 @@
+#include <dlfcn.h>
+
+#include "linux.h"
+
+AqKanji2Koe::AqKanji2Koe(const std::string aqKanji2KoeLibraryPath, const std::string aqUsrDicLibraryPath)
+{
+    void *aqKanji2KoeHandler = dlopen(aqKanji2KoeLibraryPath.c_str(), RTLD_LAZY);
+    if (aqKanji2KoeHandler == NULL) {
+        throw std::exception("failed load library(AqKanji2Koe)");
+    }
+    void *AqKanji2Koe_Create = dlsym(aqKanji2KoeHandler, "AqKanji2Koe_Create");
+    void *AqKanji2Koe_Create_Ptr = dlsym(aqKanji2KoeHandler, "AqKanji2Koe_Create_Ptr");
+    void *AqKanji2Koe_Release = dlsym(aqKanji2KoeHandler, "AqKanji2Koe_Release");
+    void *AqKanji2Koe_Convert = dlsym(aqKanji2KoeHandler, "AqKanji2Koe_Convert");
+    void *AqKanji2Koe_SetDevKey = dlsym(aqKanji2KoeHandler, "AqKanji2Koe_SetDevKey");
+    if (
+        AqKanji2Koe_Create == NULL ||
+        AqKanji2Koe_Create_Ptr == NULL ||
+        AqKanji2Koe_Release == NULL ||
+        AqKanji2Koe_Convert == NULL ||
+        AqKanji2Koe_SetDevKey == NULL
+    ) {
+        throw std::exception("loading library is succeeded, but can't found needed functions(AqKanji2Koe)");
+    }
+
+    void *aqUsrDicHandler = dlopen(aqUsrDicLibraryPath.c_str(), RTLD_LAZY);
+    if (aqUsrDicHandler == NULL) {
+        throw std::exception("failed load library(AqUsrDic)");
+    }
+    void *AqUsrDic_Import = dlsym(aqUsrDicHandler, "AqUsrDic_Import");
+    void *AqUsrDic_Export = dlsym(aqUsrDicHandler, "AqUsrDic_Export");
+    void *AqUsrDic_Check = dlsym(aqUsrDicHandler, "AqUsrDic_Check");
+    void *AqUsrDic_GetLastError = dlsym(aqUsrDicHandler, "AqUsrDic_GetLastError");
+    if (
+        AqUsrDic_Import == NULL ||
+        AqUsrDic_Export == NULL ||
+        AqUsrDic_Check == NULL ||
+        AqUsrDic_GetLastError == NULL
+    ) {
+        throw std::exception("loading library is succeeded, but can't found needed functions(AqUsrDic)");
+    }
+
+    m_aqKanji2KoeHandler = aqKanji2KoeHandler;
+    m_aqUsrDicHandler = aqUsrDicHandler;
+}
+
+AqKanji2Koe::~AqKanji2Koe()
+{
+	dlclose(m_aqKanji2KoeHandler);
+	dlclose(m_aqUsrDicHandler);
+}
+
+void *AqKanji2Koe::AqKanji2Koe_Create(const char *pathDic, int *pErr)
+{
+	CREATE Create = (CREATE)dlsym(m_aqKanji2KoeHandler, "AqKanji2Koe_Create");
+	return Create(pathDic, pErr);
+}
+
+void *AqKanji2Koe::AqKanji2Koe_Create_Ptr(const void *pSysDic, const void *pUserDic, int *pErr)
+{
+	CREATE Create = (CREATE)dlsym(m_aqKanji2KoeHandler, "AqKanji2Koe_Create_Ptr");
+	return Create(pSysDic, pUserDic, pErr);
+}
+
+void AqKanji2Koe::AqKanji2Koe_Release(void *hAqKanji2Koe)
+{
+	RELEASE Release = (RELEASE)dlsym(m_aqKanji2KoeHandler, "AqKanji2Koe_Release");
+	return Release(hAqKanji2Koe);
+}
+
+int AqKanji2Koe::AqKanji2Koe_Convert(void *hAqKanji2Koe, const char *kanji, char *koe, int nBufKoe)
+{
+	CONVERT Convert = (CONVERT)dlsym(m_aqKanji2KoeHandler, "AqKanji2Koe_Convert");
+	return Convert(hAqKanji2Koe, kanji, koe, nBufKoe);
+}
+
+int AqKanji2Koe::AqKanji2Koe_SetDevKey(const char *devKey)
+{
+	SETKEY SetKey = (SETKEY)dlsym(m_aqKanji2KoeHandler, "AqKanji2Koe_SetDevKey");
+	return SetKey(key);
+}
+
+int AqKanji2Koe::AqUsrDic_Import(const char *pathUserDic, const char *pathDicCsv)
+{
+	IO Import = (IO)dlsym(m_aqUsrDicHandler, "AqUsrDic_Import");
+	return Import(pathUserDic, pathDicCsv);
+}
+
+int AqKanji2Koe::AqUsrDic_Export(const char *pathUserDic, const char *pathDicCsv)
+{
+	IO Export = (IO)dlsym(m_aqUsrDicHandler, "AqUsrDic_Export");
+	return Export(pathUserDic, pathDicCsv);
+}
+
+int AqKanji2Koe::AqUsrDic_Check(const char *surface, const char *yomi, int posCode)
+{
+	CHECK Check = (CHECK)dlsym(m_aqUsrDicHandler, "AqUsrDic_Check");
+	return Check(surface, yomi, posCode);
+}
+
+const char *AqKanji2Koe::AqUsrDic_GetLastError()
+{
+	GETERROR GetError = (GETERROR)dlsym(m_aqUsrDicHandler, "AqUsrDic_GetLastError");
+	return GetError();
+}
---
+++ aqkanji2koe/linux.h
@@ -0,0 +1,35 @@
+#ifndef LINUX_H
+#define LINUX_H
+
+#include <napi.h>
+#include <string>
+
+#include "common.h"
+
+class AqKanji2Koe {
+public:
+    AqKanji2Koe(const std::string aqKanji2KoeLibraryPath, const std::string aqUsrDicLibraryPath);
+    ~AqKanji2Koe();
+
+    void *AqKanji2Koe_Create(const char *pathDic, int *pErr);
+    void *AqKanji2Koe_Create_Ptr(const void *pSysDic, const void *pUserDic, int *pErr);
+
+    void AqKanji2Koe_Release(void *hAqKanji2Koe);
+
+    int AqKanji2Koe_Convert_utf8(void *hAqKanji2Koe, const char *kanji, char *koe, int nBufKoe);
+    int AqKanji2Koe_Convert_utf16(void *hAqKanji2Koe, const char16_t *kanji, char16_t *koe, int nBufKoe);
+    int AqKanji2Koe_Convert_sjis(void *hAqKanji2Koe, const char *kanji, char *koe, int nBufKoe);
+
+    int AqKanji2Koe_SetDevKey(const char *devKey);
+
+    int AqUsrDic_Import(const char *pathUserDic, const char *pathDicCsv);
+    int AqUsrDic_Export(const char *pathUserDic, const char *pathDicCsv);
+    int AqUsrDic_Check(const char *surface, const char *yomi, int posCode);
+
+    const char *AqUsrDic_GetLastError();
+private:
+    void *m_aqKanji2KoeHandler;
+    void *m_aqUsrDirHandler;
+};
+
+#endif // LINUX_H
